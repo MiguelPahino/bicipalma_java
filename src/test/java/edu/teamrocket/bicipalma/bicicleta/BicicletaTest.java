ec82e34ace005e89d69e41e810e4014437aca653
MiguelPahino <mpahinogutierrez@gmail.com>
2026-04-09 23:55:44 +0200

    test(test): :white_check_mark: Añadir los test

---
+++ src/test/java/edu/teamrocket/bicipalma/bicicleta/BicicletaTest.java
@@ -0,0 +1,37 @@
+package test.java.edu.teamrocket.bicipalma.bicicleta;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Before;
+import org.junit.jupiter.api.Test;
+
+public class BicicletaTest {
+
+    private static Bicicleta bici = null;
+
+    @Before
+    public void setup_bicicleta() {
+        // ARRANGE
+        bici = new Bicicleta(101);
+    }
+    
+    @Test
+    public void test_constructor_bicicleta() {
+        // ARRANGE
+        int idExpected = 101;
+        // ACT
+        int id = bici.getId();
+        // ASSERT
+        assertEquals(idExpected, id);
+    }
+
+    @Test
+    public void test_toString_bicicleta() {
+        // ARRANGE
+        String id = "101";
+        // ACT
+        String representacionIdBici = bici.toString();
+        // ASSERT
+        assertEquals(id, representacionIdBici);
+    }
+}
