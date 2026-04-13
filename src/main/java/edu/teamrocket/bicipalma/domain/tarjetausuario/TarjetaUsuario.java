30cce8dc34e3b846c58ea01b65ec159d9258bb23
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2026-04-13 12:59:41 +0200

    feat(TarjetaUsuario): :sparkles: Añadir la clase TarjetaUsuario al proyecto.

--- src/main/java/edu/teamrocket/bicipalma/domain/tarjetausuario/TarjetaUsuario.java
+++ src/main/java/edu/teamrocket/bicipalma/domain/tarjetausuario/TarjetaUsuario.java
@@ -1,1 +1,26 @@
 package edu.teamrocket.bicipalma.domain.tarjetausuario;
+
+public class TarjetaUsuario implements Autenticacion {
+    private String id;
+    private boolean activada = false;
+
+    TarjetaUsuario(String id_inicio, Boolean activo) {
+        this.id = id_inicio;
+        this.activada = activo;
+    }
+
+    @Override
+    public Boolean isActivada() {
+        return this.activada;
+    }
+
+    public void setActivada(Boolean estado) {
+        this.activada = estado;
+    }
+
+    @Override
+    public String toString() {
+        return this.id;
+    }
+
+}
